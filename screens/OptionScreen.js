48823b68de2762bd5c8043311c67225d0278ae78
Ugo Tiberto <ugo.tiberto@gmail.com>
2023-07-28 17:36:49 +0200

    Dark Maud

--- screens/OptionScreen.js
+++ screens/OptionScreen.js
@@ -2,3 +2,28 @@
 // dispatch(switchMode())
 
 // const isDarkMode = useSelector(state => state.darkmode.value)
+
+// <View style={[styles.sameStyle, isDarkMode ? styles.darkBg : styles.lightBg]}>
+// <Text style={[styles.sameStyle, isDarkMode ? styles.darkText : styles.lightText]}>Exemple</Text>
+// </View>
+
+/* 
+sameStyle: {
+    height: 200,
+    width: '100%'
+    justifyContent: 'center',
+    alignItems: 'center'
+},
+darkBg: {
+    backgroundColor: '#000'
+},
+lightBg: {
+    backgroundColor: '#f2f2f2'
+},
+darkText: {
+    color: '#fff'
+},
+lightText: {
+    color: '#000'
+}
+ */

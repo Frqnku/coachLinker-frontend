54007b124288fe54d7eb5f82f6cb3ee8ccfef434
Ugo Tiberto <ugo.tiberto@gmail.com>
2023-07-28 16:22:01 +0200

    Configuration du redux persist

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit'; // Update import
+import { persistStore, persistReducer } from 'redux-persist'; // Import Redux Persist
+import { PersistGate } from 'redux-persist/integration/react';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStoragee
+
+// Redux Persist Config
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist: ['user']
+};
+const rootReducer = combineReducers({
+  //user: user
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+});
+const persistor = persistStore(store);
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 }
 

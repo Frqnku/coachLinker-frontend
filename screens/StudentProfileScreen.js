be6ff466c5fb3cfc8b07fa34209b728b56aead44
Laurent Boucid <laurent.boucid@hotmail.fr>
2023-07-31 12:00:07 +0200

    test

--- screens/StudentProfileScreen.js
+++ screens/StudentProfileScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {KeyboardAvoidingView, Image, TextInput, View} from 'react-native';
+import {KeyboardAvoidingView, Image, TextInput, View, StyleSheet} from 'react-native';
 
 export default function StudentProfileScreen() {
   return (

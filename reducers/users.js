7dd01ee2ec263e13b6f4c98f7983ab9c63c15226
Ugo Tiberto <ugo.tiberto@gmail.com>
2023-08-01 17:02:10 +0200

    Résolution conflit reducer users

--- reducers/users.js
+++ reducers/users.js
@@ -1,24 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// problème avec value dans initialState à chercher.
-
 const initialState = {
-<<<<<<< HEAD
-  // value: {
-  //   currentLocation: {
-  //       latitude: 0,
-  //       longitude: 0
-  //     },
-  //     statusGranted: true
-  // }
-
-  currentLocation: {
-    latitude: 0,
-    longitude: 0
-  },
-  statusGranted: true
-      
-=======
     value: {
       currentLocation: {
         latitude: 48.8877611,
@@ -31,8 +13,7 @@ const initialState = {
       },
       photo: '../assets/utilisateur.png',
       statusGranted: true
-}
->>>>>>> f591de83ac5b5834c920f11abf4322b97d1d3d4f
+    }
 };
 
 export const userSlice = createSlice({
@@ -40,8 +21,7 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     updateCurrentLocation: (state, action) => {
-        // state.value.currentLocation = action.payload; 
-        state.currentLocation = action.payload;
+        state.value.currentLocation = action.payload;
     },
     updateSearchLocation: (state, action) => {
         state.value.searchLocation = action.payload;
@@ -50,11 +30,6 @@ export const userSlice = createSlice({
         state.value.searchLocation = action.payload;
     },
     updateStatus: (state, action) => {
-<<<<<<< HEAD
-        // state.value.statusGranted = action.payload
-        state.statusGranted = action.payload;
-    }
-=======
         state.value.statusGranted = action.payload
     },
     addPhoto: (state, action) => {

f8ada44dc3136bd0aede90c0383960f706f22f80
Maud <ali-5@live.fr>
2023-08-03 11:47:13 +0200

    save before pull

--- screens/ConnexionScreen.js
+++ screens/ConnexionScreen.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, Pressable, TextInput, TouchableOpacity, KeyboardAvoidingView, Modal } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, TextInput, TouchableOpacity, KeyboardAvoidingView, Modal } from 'react-native';
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout } from '../reducers/people';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ConnexionScreen({ navigation }) {
     const dispatch = useDispatch();
@@ -61,13 +62,26 @@ export default function ConnexionScreen({ navigation }) {
 
     return (
         <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <TextInput placeholder="Email" onChangeText={(value) => setSignUpEmail(value)} value={signUpEmail} style={styles.input} />
-            <TextInput placeholder="Mot de passe" onChangeText={(value) => setSignUpPassword(value)} value={signUpPassword} style={styles.input} secureTextEntry={true}/>
+        <LinearGradient 
+              colors={['black','#FF6100']} 
+              start={{
+                x: 0.2,
+                y: 0.3,
+              }}
+              end={{
+                x: -0.3,
+                y: -0.3
+              }} style={styles.box}>
+                 <View>
+                 <Image style={styles.image} source={require('../assets/logowhite.png')} />
+                 </View>
+            <TextInput placeholderTextColor={"#505050"} placeholder="Email" onChangeText={(value) => setSignUpEmail(value)} value={signUpEmail} style={styles.input} />
+            <TextInput placeholderTextColor={"#505050"}  placeholder="Mot de passe" onChangeText={(value) => setSignUpPassword(value)} value={signUpPassword} style={styles.input} secureTextEntry={true}/>
             <TouchableOpacity onPress={() => handleSignup()} style={styles.button} activeOpacity={0.8}>
                 <Text style={styles.textButton}>S'inscrire</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => handleModal()} activeOpacity={0.8}>
-                <Text style={styles.signin}>Déjà inscrit ?</Text>
+                <Text style={styles.signin}>T'es déjà inscrit ? Let's go !</Text>
             </TouchableOpacity>
             <Modal  animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => { Alert.alert('Modal has been closed.');
                     setModalVisible(!modalVisible); }}>
@@ -84,42 +98,66 @@ export default function ConnexionScreen({ navigation }) {
                     </View>
                 </View>
             </Modal>
+           </LinearGradient>
         </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
+
+  image:{
+    width: 250,
+    height: 250,
+    marginTop: 45,
+    borderRadius: 100,
+  },
     container: {
         flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
     },
+    box:{
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      }, 
     input: {
+        marginTop: 25,
         fontSize : 20,
-        backgroundColor: "#F2F2F2",
+        backgroundColor: '#2E2E2E',
         width : "80%",
         margin : "4%",
         height: 40,
-        borderRadius: 5,
-        paddingLeft: 5
+        borderRadius: 15,
+        paddingLeft: 5,
       },
       button: {
         justifyContent: 'center',
         alignItems: 'center',
-        width: '50%',
-        height: 50,
-        backgroundColor: '#F4A100',
-        borderRadius: 5,
-        marginTop: 15
+        width: '30%',
+        height: 40,
+        backgroundColor: '#BF5000',
+        borderRadius: 25,
+        marginTop: 15,
+        shadowOffset: {
+          width: 0,
+          height: 5,
+        },
+        shadowColor: 'red', 
       },
       textButton: {
-        fontSize : 20,
-        color: '#ffffff',
+        fontSize : 15,
+        color: 'black',
+        fontWeight: 'bold',
       },
       signin: {
-        fontSize : 20,
-        marginTop: 50
+        fontSize : 22,
+        marginTop: 50,
+        color : 'black',
+        fontWeight: 'bold',
+        // backgroundColor: '#58FD0B',
+        textShadowColor: 'white',  //'rgba(255, 165, 0, 1)', Couleur de l'ombre (noir avec opacité 0.75)
+        textShadowOffset: { width: 0.5, height: 0.5 }, // Décalage de l'ombre (effet relief)
+        textShadowRadius: 20, // Rayon de l'ombre (effet relief)
       },
       centeredView: {
         flex: 1,
@@ -156,8 +194,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: 150,
         height: 50,
-        backgroundColor: '#F4A100',
         borderRadius: 5,
-        marginTop: 15
+        marginTop: 15,
+        backgroundColor: '#BF5000',
+        shadowOffset: {
+          width: 0,
+          height: 5,}
       }
 })

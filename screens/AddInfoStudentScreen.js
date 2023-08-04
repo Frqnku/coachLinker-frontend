9c2723edc7ccf932b6b5edd9ada866454ae670a2
Maud <ali-5@live.fr>
2023-08-04 10:40:33 +0200

    save before pull

--- screens/AddInfoStudentScreen.js
+++ screens/AddInfoStudentScreen.js
@@ -16,18 +16,15 @@ export default function AddInfoStudentScreen({navigation}) {
 
   const dispatch = useDispatch()
   const isFocused = useIsFocused();
- 
   const isDarkMode = useSelector(state => state.darkMode.value)
-
   const user = useSelector((state) => state.users.value);
+  const student = useSelector((state) => state.users.value) 
 
   const [studentName, setStudentName] = useState('')
   const [studentFirstname, setStudentFirstname] = useState('')
   const [studentDateOfBirth, setStudentDateOfBirth] = useState('')
   const [studentMyDescription, setStudentMyDescription] = useState('')
-//   const [studentImage, setStudentImage] = useState('')
   const [studentSports, setStudentSports] = useState([])
-
   const [selectedImages, setSelectedImages] = useState([]);
   
   // const camera : 
@@ -36,8 +33,6 @@ export default function AddInfoStudentScreen({navigation}) {
   const [flashMode, setFlashMode] = useState(FlashMode.off);
 
   let cameraRef = useRef(null);
-
-  const student = useSelector((state) => state.users.value) 
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -56,7 +51,7 @@ export default function AddInfoStudentScreen({navigation}) {
           type: 'image/jpeg',
         });
 
-        fetch('http://coach-linker-backend.vercel.app/upload', {
+        fetch('https://coach-linker-backend.vercel.app/upload', {
           method: 'POST',
           body: formData,
         }).then((response) => response.json())
@@ -72,14 +67,15 @@ export default function AddInfoStudentScreen({navigation}) {
 
 const handleValidate = async () => {
   console.log(studentSports);
-  try {
+  try { 
+    const favoriteSport = useSelector((state) => state.student.value.favoriteSport);
     await dispatch(signUp({
       name: studentName, 
       firstname: studentFirstname,
       dateOfBirth: studentDateOfBirth,
       myDescription: studentMyDescription,
       image: user.photo,
-      favoriteSport: studentSports,
+      favoriteSport: favoriteSport
     }));
 
     const signUpData = {
@@ -90,10 +86,10 @@ const handleValidate = async () => {
       dateOfBirth: studentDateOfBirth,
       myDescription: studentMyDescription,
       image: user.photo,
-      favoriteSport: studentSports,
+      favoriteSport: favoriteSport
     };
     
-    const response = await fetch('http://coach-linker-backend.vercel.app/students/new', {
+    const response = await fetch('https://coach-linker-backend.vercel.app/students/new', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(signUpData),
@@ -135,6 +131,7 @@ const handleImageSelect = (image, imageName) => {
   };
 
 
+
     const requestCameraPermission = async () => { 
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
@@ -149,7 +146,7 @@ const handleImageSelect = (image, imageName) => {
       type: 'image/jpeg',
     });
    
-    fetch('http://coach-linker-backend.vercel.app/upload', {
+    fetch('https://coach-linker-backend.vercel.app/upload', {
       method: 'POST',
       body: formData,
     }).then((response) => response.json())
@@ -175,10 +172,10 @@ const LightEnd = {x : 0.3, y : 0.1};
 return (
     <KeyboardAvoidingView style={[styles.container, isDarkMode ? styles.darkBg : styles.lightBg]} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
         <LinearGradient
-    colors={isDarkMode ? DARK_COLORS : LIGHT_COLORS}
-    start={isDarkMode ? DarkStart : LightStart}
-    end={isDarkMode ? DarkEnd : LightEnd}
-    style={styles.background} >
+            colors={isDarkMode ? DARK_COLORS : LIGHT_COLORS}
+            start={isDarkMode ? DarkStart : LightStart}
+            end={isDarkMode ? DarkEnd : LightEnd}
+            style={styles.background} >
         <GoodMorning/>
         <Image style={[styles.return, isDarkMode ? styles.darkReturn : styles.lightReturn]} source={require('../assets/bouton-retour.png')} />
     
@@ -305,8 +302,8 @@ const styles = StyleSheet.create({
   container : {
     flex :1 ,
     backgroundColor: '#E8E8E8',
-    justifyContent: "space-evenly",   
-    },  
+    justifyContent: "space-evenly",
+    },
   background:{
       width: "100%",
       height: "100%",
@@ -332,12 +329,12 @@ const styles = StyleSheet.create({
     darkInput:{
         backgroundColor: '#505050',
         borderColor: "#505050",
-        
+
     },
     lightInput:{
         backgroundColor: '#E8E8E8',
         borderColor: "#E8E8E8",
-        
+
     },
     darkImg:{
         backgroundColor: '#2E2E2E',
@@ -361,12 +358,12 @@ const styles = StyleSheet.create({
         marginTop: "8%",
         borderRadius: 50,
     },
-    
+
     picture : {
         justifyContent: "center",
         flexDirection: 'row',
         marginTop: "2%",
-        
+
     },
     image :{
         width:100,
@@ -374,163 +371,162 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         borderRadius: 50,
 
-    },
-    crayon :{
-        width:20,
-        height:20,
-    },
-    inputs: {
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginTop: "8%",
-        marginLeft: "5%",
-        width: "90%",
-        backgroundColor:"#fff",
-        borderRadius: 5,
-        
-    },
-    inputNom: {
-        fontSize : 20,
-        borderColor: "#E8E8E8",
-        borderWidth: 2,
-        width : "80%",
-        margin : "4%",
-        height: 40,
-        paddingLeft: 5,
-        borderRadius: 5,
-        
-    },
+},
+crayon :{
+    width:20,
+    height:20,
+},
+inputs: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: "8%",
+    marginLeft: "5%",
+    width: "90%",
+    backgroundColor:"#fff",
+    borderRadius: 5,
     
-    inputPrenom :{
-        fontSize : 20,
-        borderColor: "#E8E8E8",
-        borderWidth: 2,
-        width : "80%",
-        margin : "4%",
-        height: 40,
-        paddingLeft: 5,
-        borderRadius: 5,
-    },
-    inputDate :{
-        fontSize : 20,
-        borderColor: "#E8E8E8",
-        borderWidth: 2,
-        width : "80%",
-        margin : "4%",
-        height: 40,
-        paddingLeft: 5,
-        borderRadius: 5,
-    },
-    description :{
-        alignItems: 'center',
-        marginTop: "8%",
-        marginLeft: "5%",
-        width: "90%",
-        backgroundColor:"#fff",
-        borderRadius: 5,
-        
-    },
-    inputMoi :{
-        fontSize : 20,
-        alignItems:'flex-start',
-        borderColor: "#E8E8E8",
-        borderWidth: 2,
-        width : "80%",
-        margin : "4%",
-        height: 150,
-        paddingLeft: 5,
-        borderRadius: 5,
-        paddingBottom: 100,
-    },
-    favoris :{
-        fontSize:20,
-        marginTop: "8%",
-        paddingLeft: 20,
-        color: "#7B7B7B",
-    },
+},
+inputNom: {
+    fontSize : 20,
+    borderColor: "#E8E8E8",
+    borderWidth: 2,
+    width : "80%",
+    margin : "4%",
+    height: 40,
+    paddingLeft: 5,
+    borderRadius: 5,
     
-    scroll:{
-        marginLeft: 40,
-        marginRight : 40,
-      },
-    logos :{
-        margin: 20,
-        height:70,
-        width :90,
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      sportIcon: {
-        width:60,
-        height:60,
-    },
-    sports: {
-      display: 'none'
-    },
-    itemName: {
-        fontWeight: 'bold',
-        marginRight: 100,
-      },
-     removeButton: {
-        color: 'black',
-        fontWeight: 'bold',
-        marginLeft: 10,
-        fontSize: 16,
-      },
-      selectedImageContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginVertical: 5,
-      },
-      selectedImagesContainer: {
-        marginVertical: 10,
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF',
-        width: 350,
-      },
-    camera: {
-        flex: 1,
-        },
-        buttonsContainer: {
-        flex: 0.1,
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'space-between',
-        paddingTop: 20,
-        paddingLeft: 20,
-        paddingRight: 20,
-        },
-        button: {
-        width: 44,
-        height: 44,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        borderRadius: 50,
-        },
-        snapContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        paddingBottom: 25,
-        },
-        input2: {
-        fontSize : 20,
-        backgroundColor: "#F2F2F2",
-        width : 200,
-        margin : "4%",
-        height: 40,
-        borderRadius: 5,
-        paddingLeft: 5
-        },
-        button2: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 150,
-        height: 50,
-        backgroundColor: '#F4A100',
-        borderRadius: 5,
-        marginTop: 15
-        }
-});
+},
 
+inputPrenom :{
+    fontSize : 20,
+    borderColor: "#E8E8E8",
+    borderWidth: 2,
+    width : "80%",
+    margin : "4%",
+    height: 40,
+    paddingLeft: 5,
+    borderRadius: 5,
+},
+inputDate :{
+    fontSize : 20,
+    borderColor: "#E8E8E8",
+    borderWidth: 2,
+    width : "80%",
+    margin : "4%",
+    height: 40,
+    paddingLeft: 5,
+    borderRadius: 5,
+},
+description :{
+    alignItems: 'center',
+    marginTop: "8%",
+    marginLeft: "5%",
+    width: "90%",
+    backgroundColor:"#fff",
+    borderRadius: 5,
+    
+},
+inputMoi :{
+    fontSize : 20,
+    alignItems:'flex-start',
+    borderColor: "#E8E8E8",
+    borderWidth: 2,
+    width : "80%",
+    margin : "4%",
+    height: 150,
+    paddingLeft: 5,
+    borderRadius: 5,
+    paddingBottom: 100,
+},
+favoris :{
+    fontSize:20,
+    marginTop: "8%",
+    paddingLeft: 20,
+    color: "#7B7B7B",
+},
+
+scroll:{
+    marginLeft: 40,
+    marginRight : 40,
+  },
+logos :{
+    margin: 20,
+    height:70,
+    width :90,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sportIcon: {
+    width:60,
+    height:60,
+},
+sports: {
+  display: 'none'
+},
+itemName: {
+    fontWeight: 'bold',
+    marginRight: 100,
+  },
+ removeButton: {
+    color: 'black',
+    fontWeight: 'bold',
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  selectedImageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  selectedImagesContainer: {
+    marginVertical: 10,
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    width: 350,
+  },
+camera: {
+    flex: 1,
+    },
+    buttonsContainer: {
+    flex: 0.1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    paddingTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    },
+    button: {
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    borderRadius: 50,
+    },
+    snapContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 25,
+    },
+    input2: {
+    fontSize : 20,
+    backgroundColor: "#F2F2F2",
+    width : 200,
+    margin : "4%",
+    height: 40,
+    borderRadius: 5,
+    paddingLeft: 5
+    },
+    button2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 50,
+    backgroundColor: '#F4A100',
+    borderRadius: 5,
+    marginTop: 15
+    }
+});

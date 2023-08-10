de6bf39e83f5039b4264b9790d1132dc77876cfb
yacine <yacine.lariane@gmail.com>
2023-08-10 10:43:27 +0200

    CleanScreens

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity,} from 'react-native';
 import { useSelector } from 'react-redux';
 import { LinearGradient } from 'expo-linear-gradient';
 
